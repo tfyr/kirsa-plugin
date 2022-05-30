8b85087523d10a5ba0f1c217b66a3c4e15923a10
nash <nash34@gmail.com>
2022-05-31 03:32:59 +0500

    1

--- download.sh
+++ download.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+cd ~/kirsa-plugin
 git checkout master
 git pull
 source ~/env/bin/activate
-python download.py --utm-url=http://localhost:8080
+date >> download.log
+python download.py --utm-url=http://localhost:8080 >> download.log
 deactivate
