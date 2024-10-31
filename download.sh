dadf935b7cbd6981c9a79e0853f50cb2006e7cd5
Nail <nash34@gmail.com>
2024-10-31 18:24:07 +0500

    pip3 install lxml

--- download.sh
+++ download.sh
@@ -3,6 +3,7 @@ cd ~/kirsa-plugin
 git checkout master
 git pull
 source ~/env/bin/activate
+pip3 install lxml
 date >> download.log
 python download.py --utm-url=http://localhost:8080 >> download.log
 deactivate
