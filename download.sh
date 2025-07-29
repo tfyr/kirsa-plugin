f5ab25aba6606fbdd0dd962430d9a50d4a0aa0a5
Nail <nash34@gmail.com>
2025-07-29 13:00:29 +0500

    changes

--- download.sh
+++ download.sh
@@ -3,6 +3,6 @@ cd ~/kirsa-plugin
 git checkout master
 git pull
 date >> download.log
-#~/env/bin/python3 download.py --utm-url=http://localhost:8088 >> download.log
-~/kirsa-plugin/venv/venv_python/bin/python download.py --utm-url=http://localhost:8088 >> download.log
+~/env/bin/python3 download.py --utm-url=http://localhost:8080 >> download.log
+#~/kirsa-plugin/venv/venv_python/bin/python download.py --utm-url=http://localhost:8088 >> download.log
 #--backend-url=http://localhost:8015/file/
